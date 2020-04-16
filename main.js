bb85ed0707011a727b48f29f2c10ec8c06554efc
Musab <musabshakeel@gmail.com>
2020-04-16 01:08:12 -0400

    Quiz built from a .json file; fully functioning

--- main.js
+++ main.js
@@ -1,29 +1,14 @@
 // Constants
-var num_questions = 4;
+var num_questions;
 var error = {text: "Ooops! You missed a question! Go back and answer all the questions!", 
             bg: "url(images/error.gif)"};
-var harvey = {text: "You are Harvey Specter!", bg: "url(images/harvey.gif" };
-var louis = {text: "You are Louis Litt!", bg: "url(images/louis.gif" };
-var donna = {text: "You are Donna Paulsen!", bg: "url(images/donna.gif" };
-var jessica = {text: "You are Jessica Pearson!", bg: "url(images/jessica.gif" };
-var michael = {text: "You are Michael Ross!", bg: "url(images/michael.gif" };
-var rachel = {text: "You are Rachel Zane!", bg: "url(images/rachel.gif" };
+var harvey = {text: "You are Harvey Specter!", bg: "url(images/harvey.gif)" };
+var louis = {text: "You are Louis Litt!", bg: "url(images/louis.gif)" };
+var donna = {text: "You are Donna Paulsen!", bg: "url(images/donna.gif)" };
+var jessica = {text: "You are Jessica Pearson!", bg: "url(images/jessica.gif)" };
+var michael = {text: "You are Michael Ross!", bg: "url(images/michael.gif)" };
+var rachel = {text: "You are Rachel Zane!", bg: "url(images/rachel.gif)" };
 
-
-/* 
- * Defining select state for choice images/text
- *
- */
-$('.choice-img').on('click', function(e) {
-
-    let img_id = e.target.id;
-    let question_num = `q${e.target.id[1]}`;
-    console.log(img_id);
-        
-    $(`.${question_num}.choices-container div.choice-img`).css("opacity", ".2");
-    $(`.${question_num}.choices-container div[id="${e.target.id}"]`).css("opacity", "1");
-
-});
 
 /*
  * Given an array of integers, find and return the mode.
@@ -59,6 +44,105 @@ function mode(numbers) {
     return modes;
 }
 
+/* Loading quiz through JSON file */
+
+// Pre-condition: 
+// - At least one question provided
+// - At least some answers provided for any given question
+$.getJSON("data1.json", function(data) {
+    // now you can do something with this data. 
+    // remember you can only work with the data in this callback
+    // data.title has the title
+    // maybe you want to loop through data.questions? 
+    num_questions = data.questions.length;
+    // Add quiz title in header
+    $("div.header h1").text(data.title);
+
+    // Append submit button and modal text
+    var submit_button = `
+    <div class="submit-button-container">
+        <button type="button" id="submit-button"><span>What am I?</span></button>
+    </div>
+    `
+    var modal_outcome = `
+    <div id="myModal" class="outcome-container">
+        <div class="outcome-content">
+            <div id=outcome-content-gif></div>
+            <div id="outcome-content-text" ></div>
+        </div>
+    </div>
+    `
+    $(".main").append(submit_button);
+    $(".main").append(modal_outcome);
+
+    // Build string containing all questions
+    var questions = "";
+    for (let i=0; i < data.questions.length; i++) {;
+        var q = data.questions[i]
+
+        // Build string containing one question
+        var question_string = `
+        <div class="${q.question_name} question-wrapper">
+            <div class="${q.question_name} question-text-container" style='background-image: url(${q.question_img_url})'>
+                <h2> ${q.question_text} </h2>
+            </div>
+        `;
+        question_string += `
+            <div class="${q.question_name} choices-container">
+        `;
+
+        // Loop through answer list
+        for (let j=0; j < q.answers.length; j++) {
+            let ans = q.answers[j];
+            question_string += `
+                <label>
+                    <div id="${q.question_name}.${j+1}" class="choice-img" style="background-image: url(${ans.img_url})">
+                        <div class="q-text"> ${ans.text} </div>
+                    </div>
+                    <input type="radio" name="${q.question_name}" value="${ans.outcome}">
+                </label>
+            `;
+        }
+        question_string += `
+            </div>
+        </div>
+        `;
+        console.log(question_string); // Print each question string
+        questions += question_string;
+    }
+
+    console.log(questions); // Print overall question stirng
+    $(".main").prepend(questions);
+
+/* 
+ * Defining select state for choice images/text
+ *
+ */
+$('.choice-img').on('click', function(e) {
+
+    let img_id = e.target.id;
+    let question_num = `q${e.target.id[1]}`;
+    console.log(img_id);
+
+    console.log("HEYYYY");
+        
+    $(`.${question_num}.choices-container div.choice-img`).css("opacity", ".2");
+    $(`.${question_num}.choices-container div[id="${e.target.id}"]`).css("opacity", "1");
+
+});
+
+/*
+ * Adapted from: https://www.w3schools.com/howto/howto_css_modals.asp
+ * Closes modal box when user clicks outside it.
+ */
+var modal = document.getElementById("myModal");
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 /* 
  * Actions for when submit button is clicked. Determines and outputs the Suits character 
  * based off the user's responses. Displays an error to the user if not all questions are
@@ -78,61 +162,44 @@ $('#submit-button').on('click', function(e) {
     var modes = mode(choices);
     console.log("choices modes: " + modes);
 
-    if (choices.length != num_questions) {
-        console.log("ERROR");
-        $('.outcome-container').css("display", "flex");
-        $('#outcome-content-gif').css("background-image", error.bg)
-        $('#outcome-content-text').text(error.text);
+    var outcome_bg;
+    var outcome_text;
 
+    if (choices.length != num_questions) {
+        outcome_bg = error.bg;
+        outcome_text = error.text;
     }
     else if (modes.includes(1) && modes.includes(2)) {
-        console.log('You are Michael');
-        $('.outcome-container').css("display", "flex");
-        $('#outcome-content-gif').css("background-image", michael.bg)
-        $('#outcome-content-text').text(michael.text);
+        outcome_bg = michael.bg;
+        outcome_text = michael.text;
     }
     else if (modes.includes(3) && modes.includes(4)) {
-        console.log(rachel.text);
-        $('.outcome-container').css("display", "flex");
-        $('#outcome-content-gif').css("background-image", rachel.bg)
-        $('#outcome-content-text').text(rachel.text);
+        outcome_bg = rachel.bg;
+        outcome_text = rachel.text;
     }
     else if (modes.includes(1)) {
-        console.log(harvey.text);
-        $('.outcome-container').css("display", "flex");
-        $('#outcome-content-gif').css("background-image", harvey.bg)
-        $('#outcome-content-text').text(harvey.text);
+        outcome_bg = harvey.bg;
+        outcome_text = harvey.text;
     }
     else if (modes.includes(2)) {
-        console.log(louis.text);
-        $('.outcome-container').css("display", "flex");
-        $('#outcome-content-gif').css("background-image", louis.bg)
-        $('#outcome-content-text').text(louis.text);
+        outcome_bg = louis.bg;
+        outcome_text = louis.text;
     }
     else if (modes.includes(3)) {
-        console.log(donna.text);
-        $('.outcome-container').css("display", "flex");
-        $('#outcome-content-gif').css("background-image", donna.bg)
-        $('#outcome-content-text').text(donna.text);
+        outcome_bg = donna.bg;
+        outcome_text = donna.text;
     }
     else if (modes.includes(4)) {
-        console.log(jessica);
-        $('.outcome-container').css("display", "flex");
-        $('#outcome-content-gif').css("background-image", jessica.bg)
-        $('#outcome-content-text').text(jessica.text);
+        outcome_bg = jessica.bg;
+        outcome_text = jessica.text;
     }
+
+    $('.outcome-container').css("display", "flex");
+    $('#outcome-content-gif').css("background-image", outcome_bg)
+    $('#outcome-content-text').text(outcome_text);
 
 
   });
 
-/*
- * Adapted from: https://www.w3schools.com/howto/howto_css_modals.asp
- * Closes modal box when user clicks outside it.
- */
-var modal = document.getElementById("myModal");
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+       
+  });
